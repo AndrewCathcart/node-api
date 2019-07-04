903ee8f3cd4c9643aba1244a2071232db1f78fd7
Andrew Cathcart <andrew.cathcart@salecycle.com>
2019-07-04 23:06:43 +0100

    file upload and dl now using multer

--- app.js
+++ app.js
@@ -2,13 +2,41 @@ const path = require("path");
 
 const express = require("express");
 const mongoose = require("mongoose");
+const multer = require("multer");
 const MONGODB_URI = require("./api-keys/api-keys").MONGODB_URI;
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + "-" + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 app.use(express.json()); // application/json
+app.use(
+  multer({
+    storage: fileStorage,
+    fileFilter: fileFilter
+  }).single("image")
+);
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 // CORS Headers
