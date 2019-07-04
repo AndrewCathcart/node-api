295040934714ab9c9330f3965f83ae56c835acee
Andrew Cathcart <andrew.cathcart@salecycle.com>
2019-07-05 00:45:08 +0100

    added auth middleware, validating using the JWT

--- routes/feed.js
+++ routes/feed.js
@@ -2,11 +2,12 @@ const express = require("express");
 const expressValidator = require("express-validator");
 
 const feedController = require("../controllers/feed");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 
 // POST /feed/post
 router.post(
