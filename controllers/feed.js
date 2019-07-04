a930cac297662e3b4c4ef8d9aa6d430a665e4903
Andrew Cathcart <andrew.cathcart@salecycle.com>
2019-07-04 23:31:44 +0100

    deleting posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -115,6 +115,26 @@ exports.updatePost = (req, res, next) => {
     });
 };
 
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error("Could not find post.");
+        error.statusCode = 404;
+        throw error;
+      }
+      //check logged in user
+      clearImage(post.imageUrl);
+      return Post.findByIdAndRemove(postId);
+    })
+    .then(result => {
+      console.log(result);
+      res.status(200).json({ message: "Successfully deleted post." });
+    })
+    .catch(err => next(err));
+};
+
 const fs = require("fs");
 const path = require("path");
 const clearImage = filePath => {
