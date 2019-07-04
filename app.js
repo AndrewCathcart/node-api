31c3c26a81f12672a36f5e0e2d56061c4d0e7c46
Andrew Cathcart <andrew.cathcart@salecycle.com>
2019-07-04 21:23:43 +0100

    adding post model, integrated mongoose (using MongoDB now)

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+const path = require("path");
+
 const express = require("express");
+const mongoose = require("mongoose");
+const MONGODB_URI = require("./api-keys/api-keys").MONGODB_URI;
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
 
 app.use(express.json()); // application/json
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 // CORS Headers
 app.use((req, res, next) => {
@@ -19,4 +24,11 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(res => {
+    app.listen(8080);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const expressValidator = require("express-validator");
 
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -20,26 +22,31 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = expressValidator.validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: "Validation failed, data that was entered is incorrect",
-        errors: errors.array()
-      });
+    return res.status(422).json({
+      message: "Validation failed, data that was entered is incorrect",
+      errors: errors.array()
+    });
   }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: {
-        name: "Andy"
-      },
-      createdat: new Date()
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/node-meme.jpg",
+    creator: {
+      name: "Andy"
     }
   });
+  post
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
