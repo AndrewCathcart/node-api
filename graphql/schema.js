11e8f24f18c58fe8c8944fc563a89ebc54dc0e25
Andrew Cathcart <andrew.cathcart@salecycle.com>
2019-07-13 21:26:56 +0100

    added some simple validation, will build on this later

--- graphql/schema.js
+++ graphql/schema.js
@@ -30,7 +30,12 @@ module.exports = buildSchema(`
     createUser(userInput: UserInputData): User!
   }
 
+  type RootQuery {
+    hello: String
+  }
+
   schema {
-      mutation:
+    query: RootQuery
+    mutation: RootMutation
   }
 `);
