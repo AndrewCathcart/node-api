318bffd6eea77b6526f147d3a5a730bb0f2b93af
Andrew Cathcart <andrew.cathcart@salecycle.com>
2019-11-09 18:31:31 +0000

    finished moving the backend over to graphql

--- graphql/schema.js
+++ graphql/schema.js
@@ -14,9 +14,9 @@ module.exports = buildSchema(`
   type User {
     _id: ID!
     email: String!
-    name: String
-    password: String!
-    status: String!
+    name: String!
+    password: String
+    status: String
     posts: [Post!]!
   }
 
@@ -31,12 +31,30 @@ module.exports = buildSchema(`
     userId: String!
   }
 
+  input PostInputData {
+    title: String!
+    content: String!
+    imageUrl: String!
+  }
+
+  type PostData {
+    posts: [Post!]!
+    totalPosts: Int!
+  }
+
   type RootQuery {
     login(email: String!, password: String!): AuthData!
+    posts(page: Int): PostData!
+    post(id: ID!): Post!
+    user: User!
   }
 
   type RootMutation {
     createUser(userInput: UserInputData): User!
+    createPost(postInput: PostInputData): Post!
+    updatePost(id: ID!, postInput: PostInputData): Post!
+    deletePost(id: ID!): Boolean
+    updateStatus(status: String!): User!
   }
 
   schema {
--- middleware/auth.js
+++ middleware/auth.js
@@ -4,22 +4,26 @@ const JWT_SECRET = require("../api-keys/api-keys").JWT_SECRET;
 module.exports = (req, res, next) => {
   const authHeader = req.get("Authorization");
   if (!authHeader) {
-    const error = new Error("Not authenticated.");
-    error.statusCode = 401;
-    throw error;
+    req.isAuth = false;
+    return next();
   }
+
   const token = req.get("Authorization").split(" ")[1];
+
   let decodedToken;
   try {
     decodedToken = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    throw err;
+    req.isAuth = false;
+    return next();
   }
+
   if (!decodedToken) {
-    const error = new Error("Not authenticated.");
-    error.statusCode = 401;
-    throw error;
+    req.isAuth = false;
+    return next();
   }
+
   req.userId = decodedToken.userId;
+  req.isAuth = true;
   next();
 };
